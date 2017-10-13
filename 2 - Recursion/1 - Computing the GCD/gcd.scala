a9bf1296248a6d320f7d18a1a353610c6a757c1e
Huy Vo <huyvohcmc@gmail.com>
2017-10-13 23:11:06 +0700

    GCD in Scala

---
+++ 2 - Recursion/1 - Computing the GCD/gcd.scala
@@ -0,0 +1,19 @@
+object Solution {
+    
+    def gcd(x: Int, y: Int): Int = {
+        if (y == 0) x else gcd(y, x % y)
+    }
+  
+
+    /**This part handles the input/output. Do not change or modify it **/
+    def acceptInputAndComputeGCD(pair:List[Int]) = {
+        println(gcd(pair.head,pair.reverse.head))
+    } 
+
+    def main(args: Array[String]) {
+        /** The part relates to the input/output. Do not change or modify it **/
+        acceptInputAndComputeGCD(readLine().trim().split(" ").map(x=>x.toInt).toList)
+    }
+    
+}
+
