387f4f6fcab8c57c857278e35f116764e08ccb32
appositum <appositum@protonmail.com>
2018-10-03 22:10:22 -0300

    Change my old username

--- 1 - Introduction/12 - Evaluating e^x/appositum.hs
+++ 1 - Introduction/12 - Evaluating e^x/appositum.hs
@@ -1,8 +1,9 @@
 round' :: RealFloat a => a -> a
 round' num = (fromIntegral $ round (num * 10000)) / 10000
 
+exp' :: (RealFloat a, Enum a) => a -> a
 exp' x = round' $ sum [x**k / (product [1..k]) | k <- [0..9]]
 
 main :: IO ()
 main = getContents >>=
-       mapM_ print. map exp'. map (read::String->Double). tail. words
+  mapM_ print . map exp' . map (read :: String -> Double) . tail . words
